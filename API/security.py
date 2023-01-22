4f280c897afb020474ba990be11db0e7ebe187f0
Gladouu <gladis.valenzuela1008@gmail.com>
2023-01-22 21:42:56 +0100

    updated with Github secret

--- API/security.py
+++ API/security.py
@@ -1,18 +1,25 @@
 import bcrypt
-from sensitive_data import *
+import os
 
-password = ADMIN_PWD
+required_envs = ['ADMIN_PWD', 'DR_SHEPERD_PWD', 'DR_SLOAN_PWD', 'TEST_PWD']
+
+for env in required_envs:
+    if env not in os.environ:
+        raise ValueError(f"{env} env var is not set.")
+
+
+password = os.environ("ADMIN_PWD")
 hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
 print(hashed_password)
 
-password = DR_SHEPERD_PWD
+password = os.environ("DR_SHEPERD_PWD")
 hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
 print(hashed_password)
 
-password = DR_SLOAN_PWD
+password = os.environ("DR_SLOAN_PWD")
 hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
 print(hashed_password)
 
-password = TEST_PWD
+password = os.environ("TEST_PWD")
 hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
 print(hashed_password)
